d6d975e7068906e7902a8a844fe841848de0563b
EyeCrown <85166673+EyeCrown@users.noreply.github.com>
2024-06-12 19:42:49 +0200

    Rework UI + change stat box behavior

--- Pendrillon/Assets/Scripts/MonoBehavior/StatsUI.cs
+++ Pendrillon/Assets/Scripts/MonoBehavior/StatsUI.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using MonoBehavior.Managers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class StatsUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
@@ -15,11 +16,18 @@ public class StatsUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     private TextMeshProUGUI _charisma;
     private TextMeshProUGUI _dexterity;
     private TextMeshProUGUI _strength;
-    private TextMeshProUGUI _composition;
+    //private TextMeshProUGUI _composition;
 
     #endregion
 
     private Animator _anim;
+
+    #endregion
+
+    #region Events
+
+    public UnityEvent DisplayStats;
+    public UnityEvent HideStats;
 
     #endregion
 
@@ -28,6 +36,9 @@ public class StatsUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     void Awake()
     {
         _anim = GetComponent<Animator>();
+        
+        DisplayStats.AddListener(OnDisplayStats);
+        HideStats.AddListener(OnHideStats);
     }
     
     void Start()
@@ -35,6 +46,8 @@ public class StatsUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
         ConnectAttributes();
         SetupStats();
         MakeObservables();
+        
+        HideStats.Invoke();
     }
 
     #endregion
@@ -45,18 +58,17 @@ public class StatsUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 
     void ConnectAttributes()
     {
-        _charisma   = transform.Find("Charisma").GetComponent<TextMeshProUGUI>();
-        _dexterity  = transform.Find("Dexterity").GetComponent<TextMeshProUGUI>();
-        _strength   = transform.Find("Strength").GetComponent<TextMeshProUGUI>();
-        _composition = transform.Find("Composition").GetComponent<TextMeshProUGUI>();
+        _charisma   = transform.Find("Charisma/Text").GetComponent<TextMeshProUGUI>();
+        _dexterity  = transform.Find("Dexterity/Text").GetComponent<TextMeshProUGUI>();
+        _strength   = transform.Find("Strength/Text").GetComponent<TextMeshProUGUI>();
     }
     
     void SetupStats()
     {
-        _charisma.text      = "CHR > " + GameManager.Instance._story.variablesState["p_char"];
-        _dexterity.text     = "DXT > " + GameManager.Instance._story.variablesState["p_stre"];
-        _strength.text      = "FRC > " + GameManager.Instance._story.variablesState["p_dext"];
-        _composition.text   = "CST > " + GameManager.Instance._story.variablesState["p_comp"];
+        _charisma.text      = GameManager.Instance._story.variablesState["p_char"].ToString();
+        _dexterity.text     = GameManager.Instance._story.variablesState["p_stre"].ToString();
+        _strength.text      = GameManager.Instance._story.variablesState["p_dext"].ToString();
+        //_composition.text   = "CST > " + GameManager.Instance._story.variablesState["p_comp"];
     }
 
     #endregion
@@ -71,17 +83,31 @@ public class StatsUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
             UpdateDexterity((int)newValue); });
         GameManager.Instance._story.ObserveVariable ("p_dext", (string varName, object newValue) => {
             UpdateStrength((int)newValue); });
-        GameManager.Instance._story.ObserveVariable ("p_comp", (string varName, object newValue) => {
-            UpdateComposition((int)newValue); });
+        // GameManager.Instance._story.ObserveVariable ("p_comp", (string varName, object newValue) => {
+        //     UpdateComposition((int)newValue); });
         
     }
     void UpdateCharisma(int newValue)    => _charisma.text      = "CHR > " + newValue;
     void UpdateDexterity(int newValue)   => _dexterity.text     = "DXT > " + newValue;
     void UpdateStrength(int newValue)    => _strength.text      = "FRC > " + newValue;
-    void UpdateComposition(int newValue) => _composition.text   = "CST > " + newValue;
+    //void UpdateComposition(int newValue) => _composition.text   = "CST > " + newValue;
 
     #endregion
 
+    #endregion
+
+    #region Event Handlers
+
+    void OnDisplayStats()
+    {
+        gameObject.SetActive(true);
+    }
+
+    void OnHideStats()
+    {
+        gameObject.SetActive(false);
+    }
+    
     #endregion
     
     #region Mouse Events
