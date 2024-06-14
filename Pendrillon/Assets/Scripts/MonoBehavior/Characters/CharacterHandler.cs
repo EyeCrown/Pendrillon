7ce62389bf1aebb8ad2a235b185ae8ba38cd9c34
EyeCrown <85166673+EyeCrown@users.noreply.github.com>
2024-06-14 19:11:21 +0200

    Update CharacterHandler.cs

--- Pendrillon/Assets/Scripts/MonoBehavior/Characters/CharacterHandler.cs
+++ Pendrillon/Assets/Scripts/MonoBehavior/Characters/CharacterHandler.cs
@@ -163,6 +163,7 @@ public class CharacterHandler : MonoBehaviour
         var judgePos = GameManager.Instance._gridScene.GetWorldPositon(new Vector2Int(2, 9));
         transform.position = new Vector3(judgePos.x, 2.75f, judgePos.z);
         _rope.SetActive(false);
+        _onStage = true;
     }
 
     #endregion
